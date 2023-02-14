b37fe6fac5a9f8292fda035500fd0d9c63b80c22
ShivamMiguel <gongamiguel@gmail.com>
2023-02-14 15:42:49 +0100

    feat:add complete CRUD

--- API/src/controllers/UserController.js
+++ API/src/controllers/UserController.js
@@ -12,6 +12,50 @@ class UserController {
       });
     }
   }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json({ body: users });
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ errors: ['ID não enviado'] });
+      }
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({ errors: ['Usuário não encontrado'] });
+      }
+      user.update(req.body);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      await user.destroy();
+      return res.status(204).json({ body: 'usuario deletado' });
+    } catch (error) {
+      return res.json(null);
+    }
+  }
 }
 
 export default new UserController();
--- API/src/routes/userRoutes.js
+++ API/src/routes/userRoutes.js
@@ -5,6 +5,10 @@ import UserController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', UserController.store);
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.patch('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
 
