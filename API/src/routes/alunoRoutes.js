7df5342fc0e4e438bfcc983cbbff3461a886c865
ShivamMiguel <gongamiguel@gmail.com>
2023-02-22 17:27:14 +0100

    chore: add login in alunos routes

--- API/src/routes/alunoRoutes.js
+++ API/src/routes/alunoRoutes.js
@@ -1,13 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Router } from 'express';
 import alunoController from '../controllers/AlunoController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
 router.get('/', alunoController.index);
 router.post('/', alunoController.store);
-router.get('/:id', alunoController.show);
-router.patch('/:id', alunoController.update);
-router.delete('/:id', alunoController.delete);
+router.get('/:id', loginRequired, alunoController.show);
+router.patch('/:id', loginRequired, alunoController.update);
+router.delete('/:id', loginRequired, alunoController.delete);
 
 export default router;
